91c0ad38579b06d4b2298409fab5173585d86a1d
shyeon.kim <kimseo@kth.se>
2013-09-27 13:53:48 +0200

    Adding codes
    Signed-off-by: shyeon.kim <kimseo@kth.se>

---
+++ testcodes/pwmset.sh
@@ -0,0 +1,13 @@
+echo am33xx_pwm > /sys/devices/bone_capemgr.7/slots
+
+#echo bone_pwm_P8_13 > /sys/devices/bone_capemgr.7/slots
+#echo bone_pwm_P8_19 > /sys/devices/bone_capemgr.7/slots
+
+#echo bone_pwm_P8_34 > /sys/devices/bone_capemgr.7/slots
+#echo bone_pwm_P8_36 > /sys/devices/bone_capemgr.7/slots
+
+#echo bone_pwm_P8_45 > /sys/devices/bone_capemgr.7/slots
+#echo bone_pwm_P8_46 > /sys/devices/bone_capemgr.7/slots
+
+echo bone_pwm_P9_14 > /sys/devices/bone_capemgr.7/slots
+#echo bone_pwm_P9_16 > /sys/devices/bone_capemgr.7/slots
