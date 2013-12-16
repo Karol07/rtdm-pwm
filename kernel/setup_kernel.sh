523f5235b9fe70f61cbf9c2055f178061786ffef
shyeon.kim <kimseo@kth.se>
2013-12-16 10:17:34 +0100

    Kernel shell patched
    Signed-off-by: shyeon.kim <kimseo@kth.se>

--- kernel/setup_kernel.sh
+++ kernel/setup_kernel.sh
@@ -6,7 +6,6 @@ git checkout -b bone26-xenomai origin/am33x-v3.8-bone26-xenomai
 git checkout 41feff4372d18015bb529554c3451554b984319f
 ./build_kernel.sh
 cd ./KERNEL
-git apply ../beaglebot/encoders/patches/0001-tieqep-driver.patch
-cd ..
-./tools/rebuild.sh
-
+git apply ../../beaglebot/encoders/patches/0001-tieqep-driver.patch
+cd ../..
+./update_kernel.sh
